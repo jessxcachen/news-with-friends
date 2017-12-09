7d5aeb8a9a24993162e1cc1f88cffcdf4a7836d3
Jessica Chen <jessicaychen@berkeley.edu>
2017-12-08 23:55:07 -0800

    finished website

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,10 +8,10 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
+      var name = $("Jessica Chen").val();
+      var email = $("jessicaychen@berkeley.edu").val();
+      var phone = $("5107704208").val();
+      var message = $("5107704208").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
